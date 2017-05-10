c1771a92e42fb672577cc161db98e1229ccac963
Critrate <Miro.Hud@gmail.com>
2017-05-10 18:44:25 +0200

    Basic javascript

---
+++ Javascript Algorithms and Data Structure/Basic Javascript/Use Conditional Logic with If Statements.js
@@ -0,0 +1,26 @@
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+  var answer;
+  if (wasThatTrue) {
+    answer = "Yes, that was true";
+  } else {
+    answer = "No, that was false";
+  }
+  return answer;
+
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);
