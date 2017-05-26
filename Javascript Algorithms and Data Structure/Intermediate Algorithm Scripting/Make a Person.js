b12214c8895c9f0a465b716b7e97a40faf56346a
Critrate <Miro.Hud@gmail.com>
2017-05-26 09:43:56 +0200

    Intermediate Algorithm Scripting

--- Javascript Algorithms and Data Structure/Intermediate Algorithm Scripting/Make a Person.js
+++ Javascript Algorithms and Data Structure/Intermediate Algorithm Scripting/Make a Person.js
@@ -1,1 +1,37 @@
+/**
+ * 
+ * 
+ * @param {String} firstAndLast 
+ * @returns 
+ */
+var Person = function(firstAndLast) {
+  // Complete the method below and implement the others similarly
 
+  let first = firstAndLast.split(' ')[0];
+  let last = firstAndLast.split(' ')[1];
+
+  this.getFullName = function() {
+    return first + ' ' + last;
+  };
+  this.getLastName = () => {
+    return last;
+  };
+  this.getFSirstName = () => {
+    return first;
+  };
+  this.setLastName = val => {
+    last = val;
+  };
+  this.setFirstName = val => {
+    first = val;
+  };
+  this.setFullName = val => {
+    first = val.split(' ')[0];
+    last = val.split(' ')[1];
+  };
+};
+
+var bob = new Person('Bob Ross');
+console.log(bob.getFullName());
+console.log(bob.setLastName('Lastname'));
+console.log(bob.getFullName());
